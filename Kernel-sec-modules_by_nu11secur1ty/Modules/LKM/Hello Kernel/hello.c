86388657d99a0d67d7320965d75ebed7f21df5eb
nu11secur1ty <venvaropt@gmail.com>
2019-09-30 10:22:30 +0300

    Update hello.c

--- Kernel-sec-modules_by_nu11secur1ty/Modules/LKM/Hello Kernel/hello.c
+++ Kernel-sec-modules_by_nu11secur1ty/Modules/LKM/Hello Kernel/hello.c
@@ -14,11 +14,11 @@
 #include <linux/kernel.h>           // Contains types, macros, functions for the kernel
 
 MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
-MODULE_AUTHOR("Derek Molloy");      ///< The author -- visible when you use modinfo
+MODULE_AUTHOR("Ventsislav Varbanovski");      ///< The author -- visible when you use modinfo
 MODULE_DESCRIPTION("A simple Linux driver for the BBB.");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");              ///< The version of the module
+MODULE_VERSION("1");                ///< The version of the module
 
-static char *name = "world";        ///< An example LKM argument -- default value is "world"
+static char *name = "Kernel";        ///< An example LKM argument -- default value is "Kernel"
 module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
 MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
 
