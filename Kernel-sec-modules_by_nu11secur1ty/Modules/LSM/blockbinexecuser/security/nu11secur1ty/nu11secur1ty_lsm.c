26c5f04f7ced0f89510cbfb58bce6b8cfc391324
nu11secur1ty <venvaropt@gmail.com>
2019-10-08 16:39:36 +0300

    Update nu11secur1ty_lsm.c

--- Kernel-sec-modules_by_nu11secur1ty/Modules/LSM/blockbinexecuser/security/nu11secur1ty/nu11secur1ty_lsm.c
+++ Kernel-sec-modules_by_nu11secur1ty/Modules/LSM/blockbinexecuser/security/nu11secur1ty/nu11secur1ty_lsm.c
@@ -9,42 +9,21 @@
  * @see https://www.nu11secur1ty.com/ for a full description and follow-up descriptions.
 */
 
-#include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
-#include <linux/module.h>           // Core header for loading LKMs into the kernel
-#include <linux/kernel.h>           // Contains types, macros, functions for the kernel
+#include <linux/init.h>
+#include <linux/module.h>
 
+MODULE_LICENSE("Dual BSD/GPL");
 
-MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
-MODULE_AUTHOR("Ventsislav Varbanovski");      ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux driver for the BBB.");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");                ///< The version of the module
-
-static char *name = "Kernel";        ///< An example LKM argument -- default value is "Kernel"
-module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
-MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
-
-/** @brief The LKM initialization function
- *  The static keyword restricts the visibility of the function to within this C file. The __init
- *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
- *  time and that it can be discarded and its memory freed up after that point.
- *  @return returns 0 if successful
- */
-static int __init nu11secur1tyBBB_init(void){
-   printk(KERN_INFO "EBB: nu11secur1ty %s from the BBB LKM!\n", name);
-   return 0;
+static int user_init(void)
+{
+    printk(KERN_ALERT "Hello, user\n");
+    return 0;
 }
 
-/** @brief The LKM cleanup function
- *  Similar to the initialization function, it is static. The __exit macro notifies that if this
- *  code is used for a built-in driver (not a LKM) that this function is not required.
- */
-static void __exit nu11secur1tyBBB_exit(void){
-   printk(KERN_INFO "EBB: Goodbye %s from the BBB LKM!\n", name);
+static void user_exit(void)
+{
+    printk(KERN_ALERT "Goodbye, dear user\n");
 }
 
-/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
- *  identify the initialization function at insertion time and the cleanup function (as
- *  listed above)
- */
-module_init(nu11secur1tyBBB_init);
-module_exit(nu11secur1tyBBB_exit);
+module_init(user_init);
+module_exit(user_exit);
