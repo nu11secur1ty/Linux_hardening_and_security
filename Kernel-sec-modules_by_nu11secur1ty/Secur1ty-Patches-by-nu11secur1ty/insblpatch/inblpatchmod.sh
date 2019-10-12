46e78c9651d9c2010bde253fcf1a3ad49ca18e27
nu11secur1ty <venvaropt@gmail.com>
2019-10-12 13:59:23 +0300

    Update inblpatchmod.sh

--- Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/inblpatchmod.sh
+++ Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/inblpatchmod.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 cd /usr/bin/
 wget https://github.com/nu11secur1ty/Linux_hardening_and_security/raw/master/Kernel-sec-modules_by_nu11secur1ty/Modules/LSM/nu11secur1ty-sec/nu11secur1ty/insblpatch
-cat > insblpatchmod.sh << EOF
+cat > insblpatchmod << EOF
 #!/usr/bin/bash
 sleep 5;
 insblpatch
 exit 0;
 EOF
-chmod a+x insblpatchmod.sh
+chmod a+x insblpatchmod
 chmod a+x insblpatch
 exit 0;
