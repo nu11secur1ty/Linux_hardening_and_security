31e2f527d7ce050517570765e01853f65265fb25
nu11secur1ty <venvaropt@gmail.com>
2019-10-14 13:46:16 +0300

    Update insmod_unload.c

--- Kernel-sec-modules_by_nu11secur1ty/Patch-working-code/insmod/insmod_unload/insmod_unload.c
+++ Kernel-sec-modules_by_nu11secur1ty/Patch-working-code/insmod/insmod_unload/insmod_unload.c
@@ -6,7 +6,5 @@ int main(void)
 {
 /* Changing the privileges for 'insmod' that user can not be using outside Linux Kernel Module */
  system("echo 1 > /proc/sys/kernel/modules_disabled");
- system("echo 'kernel.modules_disabled=1' >> /etc/sysctl.d/99-custom.conf");
-
- /* Block insmod Option by nu11secur1ty */
+/* Block insmod Option by nu11secur1ty */
 }
