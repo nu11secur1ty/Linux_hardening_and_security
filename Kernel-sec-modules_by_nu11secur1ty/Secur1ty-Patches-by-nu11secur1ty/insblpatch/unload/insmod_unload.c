9ae11cb1b9d796fdbe2d9121c5a4f0c0f4892fbf
nu11secur1ty <venvaropt@gmail.com>
2019-10-14 13:56:57 +0300

    Update insmod_unload.c

--- Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/insmod_unload.c
+++ Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/insmod_unload.c
@@ -1,1 +1,11 @@
-sx
+#include <stdio.h>
+#include <stdlib.h>
+//Author @nu11secur1ty//
+
+int main(void)
+{
+/* Unload insmod_blosk module*/
+ system("sed -i '/insblpatchmod/d' /etc/profile");
+ system("sleep 5;");
+  system("telinit 6");
+}
