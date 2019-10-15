fbbe0d5745f0a1f73b896a51d516e255142db735
nu11secur1ty <venvaropt@gmail.com>
2019-10-15 13:22:22 +0300

    Update insmod_unload.sh

--- Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/insmod_unload.sh
+++ Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/insmod_unload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # Author V.Varbanovski @nu11secur1ty
-# Copyrights reserved
+# Copyrights reserved v-1.0
 
 cd /root/
 wget https://github.com/nu11secur1ty/Linux_hardening_and_security/raw/master/Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/insmod_unload
