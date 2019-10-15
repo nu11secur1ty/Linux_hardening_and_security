63fc9c1773e39d807564d9959d6b36baaed94ab4
nu11secur1ty <venvaropt@gmail.com>
2019-10-15 13:22:57 +0300

    Update insbl_mod_patch.sh

--- Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/insbl_mod_patch.sh
+++ Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/insbl_mod_patch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # Author V.Varbanovski @nu11secur1ty
-# Copyrights reserved
+# Copyrights reserved v-1.1
 
 cd /usr/bin/
 wget https://github.com/nu11secur1ty/Linux_hardening_and_security/raw/master/Kernel-sec-modules_by_nu11secur1ty/Modules/LSM/nu11secur1ty-sec/nu11secur1ty/insblpatch
