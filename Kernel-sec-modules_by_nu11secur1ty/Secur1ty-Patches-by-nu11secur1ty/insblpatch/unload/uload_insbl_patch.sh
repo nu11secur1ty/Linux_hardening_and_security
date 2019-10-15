acb66f22673ce31f29bbabbecfc631d7f005836e
nu11secur1ty <venvaropt@gmail.com>
2019-10-15 12:57:14 +0300

    Create uload_insbl_patch.sh

---
+++ Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/uload_insbl_patch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+cd /root/
+wget https://github.com/nu11secur1ty/Linux_hardening_and_security/raw/master/Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/unload/insmod_unload
+cd /usr/bin
+  rm -rf insblpatch insblpatchmod
+  cd /root/
+  chmod a+x insmod_unload
+    ./insmod_unload
+      rm *insmod_unload*
+        sleep 3;
+ echo "Now you must reboot the system to unload completely the insmod_block module"
+exit 0;
