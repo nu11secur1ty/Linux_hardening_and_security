466386ece74190a65214e116c6f5f5c82454b55a
nu11secur1ty <venvaropt@gmail.com>
2019-10-22 15:28:24 -0400

    Update susepatch.sh

--- Sudo/suse_patch/susepatch.sh
+++ Sudo/suse_patch/susepatch.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/bash
+# Author V.Varbanovski @nu11secur1ty
+protect_priv=Defaults
+  sed -i 's/$protect_priv/#$protect_priv/' /etc/sudoers
+protect_all=ALL 
+  sed -i 's/$protect_all/#$protect_all/' /etc/sudoers
+    sed -i 's/root/#root/' /etc/sudoers
+exit 0;
