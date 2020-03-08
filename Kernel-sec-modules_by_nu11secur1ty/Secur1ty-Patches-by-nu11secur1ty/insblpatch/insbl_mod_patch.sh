6126646d3be7687ae77029a12a5953541b0fe1f9
nu11secur1ty <venvaropt@gmail.com>
2020-03-08 17:34:06 +0200

    Update insbl_mod_patch.sh

--- Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/insbl_mod_patch.sh
+++ Kernel-sec-modules_by_nu11secur1ty/Secur1ty-Patches-by-nu11secur1ty/insblpatch/insbl_mod_patch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # Author V.Varbanovski @nu11secur1ty
 # Copyrights reserved v-1.3
-
+# ExecStartPre=/bin/sleep 49
 cd /usr/bin/
 wget https://github.com/nu11secur1ty/Linux_hardening_and_security/raw/master/Kernel-sec-modules_by_nu11secur1ty/Modules/LSM/nu11secur1ty-sec/nu11secur1ty/insblpatch
 cat > insblpatchmod << EOF
@@ -19,7 +19,7 @@ chmod a+x insblpatch
 Description=insbl_mod_patch systemd service.
 
 [Service]
-ExecStartPre=/bin/sleep 49
+ExecStartPost=/bin/sleep 49
 Type=simple
 ExecStart=/bin/bash /usr/bin/insblpatchmod
 
