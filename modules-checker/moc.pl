b466035823c49ebdc9ad9e091f13e3857a80c4fb
nu11secur1ty <venvaropt@gmail.com>
2021-10-12 13:40:23 +0300

    Update moc.pl

--- modules-checker/moc.pl
+++ modules-checker/moc.pl
@@ -4,9 +4,14 @@ use warnings;
 use strict;
 use diagnostics;
 
-print"Network modules\n";
+print "Network modules\n";
+
+# all network interfaces
 my $all_net = `lshw -c network`;
+
+# wifi network interfaces
 my $wifi_net = `lshw -c network 2>&1 | grep wireless | grep driver`;
+
   print "$all_net\n";
   print "$wifi_net\n";
-  exit 0;
+    exit 0;
